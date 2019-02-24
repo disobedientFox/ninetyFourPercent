1f8016af674cb64ed796ef45c17c53d3ff2fc816
Alevtyna Yarychevska <vkysniashka@list.ru>
2019-02-24 15:18:48 +0100

    initial, add GameContext and Password Manager

---
+++ ninetyFourPercent/GameContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+
+namespace ninetyFourPercent
+{
+    class GameContext : DbContext
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public GameContext() : base("DefaultConnection")
+        {
+
+        }
+
+        #endregion
+
+        public DbSet<Trade> Trades { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Reader> Readers { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
